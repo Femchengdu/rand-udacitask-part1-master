d37e7b63938c0f6cee55a544116964a8c9b4d8b8
Ajayi Oluwafemi <FanMiao@Ajayis-MacBook-Air.local>
2016-06-05 13:34:53 +0800

    Initial solution

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,31 @@
 class TodoList
     # methods and stuff go here
+    
+    # Create a reader for the title and items	
+    attr_reader :title, :items
+    attr_writer :title
+    # Initialize todo list with a title and no items
+    def initialize(list_title)
+    	@title = list_title
+    	@items = Array.new
+    end   
+
+    # Create a new Item and adds it to the array of items
+    def add_item(new_item)
+    	item = Item.new(new_item)
+    	@items.push(item)
+    end	
 end
 
 class Item
     # methods and stuff go here
+    attr_reader :description, :completion_status
+    attr_writer :completion_status
+    # Initialize items on a todo list
+    def initialize(item_description)
+    	@description = item_description
+    	@completion_status = false
+    end	 
+
+    
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,39 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+julias_stuff = TodoList.new("Julia's stuff")
 
 # Add four new items
+julias_stuff.add_item("Do laundry")
+julias_stuff.add_item("Feed the cat")
+julias_stuff.add_item("Buy cereal")
+julias_stuff.add_item("Go dancing!")
 
 # Print the list
+puts julias_stuff.items.inspect
+puts julias_stuff.items.length
+puts julias_stuff.items[0]
 
 # Delete the first item
-
+julias_stuff.items.delete_at(0)
 # Print the list
-
+puts julias_stuff.items.inspect
 # Delete the second item
+puts julias_stuff.items.length
 
 # Print the list
+puts julias_stuff.items.inspect
 
 # Update the completion status of the first item to complete
 
+julias_stuff.items[0].completion_status = true
+
+
 # Print the list
+puts julias_stuff.items.inspect
 
 # Update the title of the list
+julias_stuff.title = "Julia's Monday morning list"
 
 # Print the list
+puts julias_stuff.items.inspect
