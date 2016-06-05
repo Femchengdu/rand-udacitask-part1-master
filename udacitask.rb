540c7a656e4f39f0a524d4d9b2985e34fa7f4d5e
Ajayi Oluwafemi <FanMiao@192.168.0.102>
2016-06-05 22:08:25 +0800

    Defined formatting methods for headding and items

--- udacitask.rb
+++ udacitask.rb
@@ -1,7 +1,35 @@
 require_relative 'todolist.rb'
+require 'artii'
+
+
+# Method to print headding in ascii art
+def print_list_headding txt
+	head = Artii::Base.new :font => 'slant'
+	puts " ---------------------------------------------------------- "
+	puts head.asciify txt
+	puts " ---------------------------------------------------------  "
+	puts "  "
+end
+
+# Method to format the items output
+def format_items items_array
+	count = 0
+	items_array.each do |block_variable|
+		description = block_variable.description + "            "
+		status = block_variable.completion_status
+		count += 1
+		puts "#{count} - #{description[0..20]}Completed: #{status}"
+	end
+end	
+
+# Method to print the list
+def print_list title, item_list
+	title
+	item_list
+end
 
 # Creates a new todo list
-julias_stuff = TodoList.new("Julia's stuff")
+julias_stuff = TodoList.new("Julia's Stuff")
 
 # Add four new items
 julias_stuff.add_item("Do laundry")
@@ -10,30 +38,28 @@ julias_stuff.add_item("Buy cereal")
 julias_stuff.add_item("Go dancing!")
 
 # Print the list
-puts julias_stuff.items.inspect
-puts julias_stuff.items.length
-puts julias_stuff.items[0]
+print_list (print_list_headding julias_stuff.title),  (format_items julias_stuff.items)
 
 # Delete the first item
 julias_stuff.items.delete_at(0)
-# Print the list
-puts julias_stuff.items.inspect
-# Delete the second item
-puts julias_stuff.items.length
 
 # Print the list
-puts julias_stuff.items.inspect
+print_list (print_list_headding julias_stuff.title),  (format_items julias_stuff.items)
+
+# Delete the second item
+julias_stuff.items.delete_at(1)
+
+# Print the list
+print_list (print_list_headding julias_stuff.title),  (format_items julias_stuff.items)
 
 # Update the completion status of the first item to complete
-
 julias_stuff.items[0].completion_status = true
 
-
 # Print the list
-puts julias_stuff.items.inspect
+print_list (print_list_headding julias_stuff.title),  (format_items julias_stuff.items)
 
 # Update the title of the list
-julias_stuff.title = "Julia's Monday morning list"
+julias_stuff.title = "My Todo List"
 
 # Print the list
-puts julias_stuff.items.inspect
+print_list (print_list_headding julias_stuff.title),  (format_items julias_stuff.items)
