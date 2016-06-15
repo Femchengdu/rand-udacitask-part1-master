8a219d978159e6f2c098fbf84de009a51b2cdcbd
Ajayi Oluwafemi <FanMiao@Ajayis-MacBook-Air.local>
2016-06-15 14:48:47 +0800

    Added function to write todo list to file

--- todolist.rb
+++ todolist.rb
@@ -1,30 +1,14 @@
 class TodoList
     # methods and stuff go here
-
-    # Setting the file paths
-    @@filepath = nil
-
-
-    def self.filepath=(path=nil)
-        @@filepath = File.join(APP_ROOT, path)
-    end 
-
-    def self.file_exists?
-        # class should know if the todo list file exists
-        if  @@filepath && File.exists?(@@filepath)
-            return true
-        else
-            return false
-        end        
-    end
     
     # Create a reader for the title and items	
-    attr_reader :title, :items
+    attr_reader :title, :items, :print_file
     attr_writer :title
     # Initialize todo list with a title and no items
     def initialize(list_title)
     	@title = list_title
     	@items = Array.new
+        @print_file = File.new("todolist.txt", "w+")
     end   
 
     # Create a new Item and adds it to the array of items
@@ -93,7 +77,8 @@ class UserInteraction
             list_object.items[position.to_i - 1].completion_status = status
         else 
             list_object.items[0].completion_status = status  
-        end     
+        end  
+        puts "\nStatus updated!! \n\n"   
     end    
 
 
@@ -102,6 +87,7 @@ class UserInteraction
         print "Type the new title: "
         new_title = gets.chomp.strip
         list_object.title = new_title
+        puts "\nTitle updated!! \n\n"
     end
 
     # Formating output to the screen
@@ -120,7 +106,7 @@ class UserInteraction
     def format_items items_array
         count = 0
         items_array.each do |block_variable|
-            description = block_variable.description + "            "
+            description = block_variable.description + "                        "
             status = block_variable.completion_status
             count += 1
             puts "#{count} - #{description[0..20]}Completed: #{status}"
@@ -131,6 +117,31 @@ class UserInteraction
     def print_list title, item_list
         print_list_headding title
         format_items item_list
+    end
+=begin
+    # Update the title of the list
+    def set_list_file list_object
+        print "Type the name of the file to write to: "
+        list_file = gets.chomp.strip
+        list_object.print_file = new_title
+    end
+=end
+
+    # Method to write the list to a file
+    def list_to_file list_object
+        puts "Writing todo list to file........."
+        # Initialize text file
+        report_file = list_object.print_file
+        # Save old value of stdout  
+        old_out = $stdout
+        # Set stdout to textfile
+        $stdout = report_file
+        # Print the todo list
+        print_list list_object.title, list_object.items
+        # Close file
+        report_file.close
+        # Reset stdout
+        $stdout = old_out
     end
 
 
@@ -158,11 +169,11 @@ class UserInteraction
             print_list list_object.title, list_object.items   
         when 'delete'
             deleting_item list_object
-
         when 'update'
-            status_update list_object
-
+            status_update list_object    
         when 'quit'
+            list_to_file list_object
+            puts "Todo list written to file!" 
             return :quit    
         else
             puts "\n Wrong command.\n"
@@ -186,6 +197,11 @@ class UserInteraction
     def introduction
         puts "\n\n<<<< Welcome to the Todo List program >>>>\n\n"
         puts "\n\n<<<<<<<This is an interactive guide to help you manage your todo list.>>>>>>>>\n\n"
+        puts "\n\n<<<< add: This adds a new item to the list>>>>\n\n"
+        puts "\n\n<<<< delete: This deletes an item from the list>>>>\n\n"
+        puts "\n\n<<<< update: This updates the completion status of an item on the list>>>>\n\n"
+        puts "\n\n<<<< title: This changes the title of the list>>>>\n\n"
+        puts "\n\n<<<< quit: This quits the interactive menu and writes the list to a file>>>>\n\n"
         puts "Hit any key followed by enter to proceed...........\n\n"
     end
     
